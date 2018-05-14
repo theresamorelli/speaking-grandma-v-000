5d7ced08c8dc6083a106fee056d479aa98481627
Theresa Morelli <theresa.l.morelli@gmail.com>
2018-05-14 23:05:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,13 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  
+
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
   else
-    "HUH?! SPEAK UP, SONNY!""
+    "HUH?! SPEAK UP, SONNY!"
+  end
 
 end
